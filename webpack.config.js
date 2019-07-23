83e6edca9a9b7796da60c71ec917313b3f75fddc
mdlulim <mduduzmdluli@gmail.com>
2019-07-23 12:00:12 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+require('webpack');
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            },
+            {
+                test: /\.(png|jpg)$/,
+                use:  "url-loader"
+            }
+
+        ]
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html'
+    })],
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
